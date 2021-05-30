87249597aabfd6491955be3061d24f48747ba070
AndreiBelich <balichandrey89@gmail.com>
2021-05-30 23:34:43 +0300

    add PropTypes for component CalendarDay

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -43,7 +43,7 @@ function Calendar() {
           week.map((day, index) => {
             return (
               <CalendarDay key={`day-${index}`}
-                           caption={day || ""}
+                           caption={`${day}` || ""}
                            isCurrentDay={day === currentDate} />
             )
           })
--- src/components/CalendarDay/index.jsx
+++ src/components/CalendarDay/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import style from "./CalendarDay.module.scss";
 
 function CalendarDay({caption, isCurrentDay}) {
@@ -6,5 +7,15 @@ function CalendarDay({caption, isCurrentDay}) {
     <div className={`${style.calendarDay} ${isCurrentDay ? style.currentDate : ""}`.trim()}>{caption}</div>
   )
 }
+
+CalendarDay.defaultProps = {
+  caption: "1",
+  isCurrentDay: false
+};
+
+CalendarDay.propTypes = {
+  caption: PropTypes.string.isRequired,
+  isCurrentDay: PropTypes.bool.isRequired
+};
 
 export default CalendarDay;

9ddf40267f25920c1ab0e737d8ffc517ad167bbb
AndreiBelich <balichandrey89@gmail.com>
2021-06-01 22:10:50 +0300

    rename class members in class CalendarDataBuilder: 1) old name "firstDay" new name "numberOfTheFirstDayOfTheMont" 2) old name "last day" new name "lastDayOfMonth"

--- src/common/js/CalendarDataBuilder.js
+++ src/common/js/CalendarDataBuilder.js
@@ -3,22 +3,25 @@ import { startOfMonth, endOfMonth, getDay, getDate } from "date-fns";
 
 class CalendarDataBuilder{
   constructor(date = Date.now()) {
+    //numberOfTheFirstDayOfTheMonth
+    //firstDay
+    //lastDay lastDayOfMonth
     this._date = date;
-    this._firstDay = getDay(startOfMonth(this._date));
-    this._lastDay = getDate(endOfMonth(this._date));
+    this._numberOfTheFirstDayOfTheMonth = getDay(startOfMonth(this._date));
+    this._lastDayOfMonth = getDate(endOfMonth(this._date));
     this._calendarBody = [];
     this._currentValue = 0;
   }
 
   setNewDate(newDate){
     this._date = newDate;
-    this._firstDay = getDay(startOfMonth(this._date));
-    this._lastDay = getDate(endOfMonth(this._date));
+    this._numberOfTheFirstDayOfTheMonth = getDay(startOfMonth(this._date));
+    this._lastDayOfMonth = getDate(endOfMonth(this._date));
   }
 
  _createFirstRow() {
     const firstRow = new Array(DAYS_PER_WEEK).fill(0);
-    for(let i = this._firstDay; i < DAYS_PER_WEEK; i++){
+    for(let i = this._numberOfTheFirstDayOfTheMonth; i < DAYS_PER_WEEK; i++){
       firstRow[i] = ++this._currentValue;
     }
     return firstRow;
@@ -51,7 +54,7 @@ class CalendarDataBuilder{
 
     this._calendarBody.push(this._createFirstRow());
 
-    const diff = this._lastDay - this._currentValue;
+    const diff = this._lastDayOfMonth - this._currentValue;
     const rows = Math.floor(diff / DAYS_PER_WEEK);
     const rest = diff % DAYS_PER_WEEK;
   
@@ -65,10 +68,3 @@ class CalendarDataBuilder{
 }
 
 export default CalendarDataBuilder;
-
-/**
- * Zaporozye, Ukraine
- * The best time to visit is September or October because weather is warm.
- * Be prepared for everything from .
- * 
- */

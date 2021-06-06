0b25a1c6e67a54501c440daa4dc1e3d7575e0fbe
AndreiBelich <balichandrey89@gmail.com>
2021-06-06 15:36:05 +0300

    add PropTypes to component Arrow

--- src/components/Calendar/Arrow/index.jsx
+++ src/components/Calendar/Arrow/index.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import style from "./Arrow.module.scss";
+import PropTypes from "prop-types";
 
 const Arrow = ({handler, direction}) => {
-  return <div onClick={handler} className={direction === "left" ? style.leftArrow : style.rightArrow}></div>
+  return <div onClick={handler} className={direction.toLowerCase() === "left" ? style.leftArrow : style.rightArrow}></div>
 }
+
+Arrow.defaultProps = {
+  handler: () => {},
+  direction : "left"
+};
+
+Arrow.propTypes = {
+  handler: PropTypes.func.isRequired,
+  direction: PropTypes.string.isRequired
+};
 
 export default Arrow;

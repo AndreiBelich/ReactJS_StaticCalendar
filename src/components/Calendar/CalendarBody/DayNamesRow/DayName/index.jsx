f9538943ec8e3b562d6dece988f3e89cdd65ad55
AndreiBelich <balichandrey89@gmail.com>
2021-06-06 17:55:34 +0300

    finish project

--- src/components/Calendar/CalendarBody/DayNamesRow/DayName/index.jsx
+++ src/components/Calendar/CalendarBody/DayNamesRow/DayName/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from "prop-types";
-import style from "./DayName.module.scss";
+import style from "./DayName.module.sass";
 
 function DayName({caption}) {
   return (
